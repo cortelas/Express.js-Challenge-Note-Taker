da47b2e85fdf0854016e31847cf6444175b50e86
cortelas <cortelas1@yahoo.com>
2021-05-30 04:34:01 -0700

    added comments

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,7 +2,13 @@ const router = require("express").Router();
 const fs = require("fs");
 const notes = require("../db/db")
 
+//gets the notes saved in db
 router.get("/api/notes", (req, res) => {
+    res.json(notes);
+});
+
+//posts the notes to db
+router.post("/api/notes", (req, res) => {
     res.json(notes);
 });
 
