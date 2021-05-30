b45790598acbb68962b5a7e967ba4d9b5e0b0996
cortelas <cortelas1@yahoo.com>
2021-05-30 04:32:01 -0700

    added get notes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,1 +1,10 @@
 const router = require("express").Router();
+const fs = require("fs");
+const notes = require("../db/db")
+
+router.get("/api/notes", (req, res) => {
+    res.json(notes);
+});
+
+
+module.exports = router;
